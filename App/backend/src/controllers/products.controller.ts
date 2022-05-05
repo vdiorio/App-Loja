086f1542e769e298afb7142974612f5ff29552d6
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-05 00:24:25 -0300

    FIX: Envolvendo controllers em "tentagatinhos"

--- App/backend/src/controllers/products.controller.ts
+++ App/backend/src/controllers/products.controller.ts
@@ -4,39 +4,63 @@ import { Request, Response } from "express";
 
 export default class ProductsService {
   static getAll = async (_req: Request, res: Response) => {
-    const products = await services.products.getAll();
-    return res.status(StatusCodes.OK).json(products);
+    try {
+      const products = await services.products.getAll();
+      return res.status(StatusCodes.OK).json(products);
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 
   static getById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const product = await services.products.getById(id);
-    if (!product) return res.status(StatusCodes.NOT_FOUND).json({ message: 'Product not found' });
-    return res.status(StatusCodes.OK).json(product);
+    try {
+      const { id } = req.params;
+      const product = await services.products.getById(id);
+      if (!product) return res.status(StatusCodes.NOT_FOUND).json({ message: 'Product not found' });
+      return res.status(StatusCodes.OK).json(product);
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 
   static getByCategory = async (req: Request, res: Response) => {
-    const { category } = req.body
-    const products = await services.products.getByCategory(category);
-    return res.status(StatusCodes.OK).json(products);
+    try {
+      const { category } = req.body
+      const products = await services.products.getByCategory(category);
+      return res.status(StatusCodes.OK).json(products);
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 
   static createProduct = async (req: Request, res: Response) => {
-    const newProduct = req.body
-    const product = await services.products.createProduct(newProduct);
-    return res.status(StatusCodes.CREATED).json(product);
+    try {
+      const newProduct = req.body
+      const product = await services.products.createProduct(newProduct);
+      return res.status(StatusCodes.CREATED).json(product);
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 
   static updateProduct = async (req: Request, res: Response) => {
-    const newProduct = req.body
-    const { id } = req.params
-    const product = await services.products.updateProduct(id, newProduct);
-    return res.status(StatusCodes.OK).json(product);
+    try {
+      const newProduct = req.body
+      const { id } = req.params
+      const product = await services.products.updateProduct(id, newProduct);
+      return res.status(StatusCodes.OK).json(product);
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 
   static deleteProduct = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const product = await services.products.deleteProduct(id);
-    return res.status(StatusCodes.NO_CONTENT).end();
+    try {
+      const { id } = req.params
+      const product = await services.products.deleteProduct(id);
+      return res.status(StatusCodes.NO_CONTENT).end();
+    } catch (e) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    }
   };
 };
