a7c569515085c56c47092d9d576552bbd04570cc
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-05 18:29:44 -0300

    NEW: Middlewares de login

--- App/backend/src/middlewares/index.ts
+++ App/backend/src/middlewares/index.ts
@@ -1,7 +1,9 @@
 import products from './products.middlewares';
 import users from './users.middlewares';
+import login from './login.middlewares';
 
 export default {
   products,
   users,
+  login,
 }
---
+++ App/backend/src/middlewares/login.middlewares.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Joi = require("joi");
+
+const loginSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export default class ProductMiddlewares {
+  static validateRequest = async (req: Request, res: Response, next: NextFunction) => {
+    const { error } = loginSchema.validate(req.body) as Joi.ValidationResult;
+    if (error) {
+      const { message } = error.details[0]
+      return res.status(StatusCodes.BAD_REQUEST).json({message});
+    }
+    next();
+  }
+}
