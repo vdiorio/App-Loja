b23d7ef58e96c05e9a4fe8f36f9d5d41d67a5833
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-04 22:54:37 -0300

    NEW: Service getById em product

--- App/backend/src/services/products.service.ts
+++ App/backend/src/services/products.service.ts
@@ -2,7 +2,12 @@ import Product from "../../models/product";
 
 export default class ProductsService {
   static getAll = async () => {
-    const products = await Product.findAll();
+    const products = await Product.findAll() as Product[];
     return products;
+  };
+  
+  static getById = async (id: string) => {
+    const product = await Product.findByPk(id) as Product;
+    return product;
   };
 };
