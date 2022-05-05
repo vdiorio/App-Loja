b68493bd9d51f5d9ad84ad2d6c603ea4848676eb
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-04 22:56:04 -0300

    NEW: Service getByCategory em product

--- App/backend/src/services/products.service.ts
+++ App/backend/src/services/products.service.ts
@@ -10,4 +10,9 @@ export default class ProductsService {
     const product = await Product.findByPk(id) as Product;
     return product;
   };
+  
+  static getByCategory = async (category: string) => {
+    const product = await Product.findAll({ where: { category } }) as Product[];
+    return product;
+  };
 };
