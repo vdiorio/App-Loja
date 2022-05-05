a97cc8565b634784051a4198cf9f45714e780893
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-05 00:14:05 -0300

    NEW: Controler update para product

--- App/backend/src/controllers/products.controller.ts
+++ App/backend/src/controllers/products.controller.ts
@@ -18,12 +18,19 @@ export default class ProductsService {
   static getByCategory = async (req: Request, res: Response) => {
     const { category } = req.body
     const products = await services.products.getByCategory(category);
-    return res.status(200).json(products);
+    return res.status(StatusCodes.OK).json(products);
   };
 
   static createProduct = async (req: Request, res: Response) => {
     const newProduct = req.body
     const product = await services.products.createProduct(newProduct);
-    return res.status(200).json(product);
+    return res.status(StatusCodes.CREATED).json(product);
+  };
+
+  static updateProduct = async (req: Request, res: Response) => {
+    const newProduct = req.body
+    const { id } = req.params
+    const product = await services.products.updateProduct(id, newProduct);
+    return res.status(StatusCodes.OK).json(product);
   };
 };
