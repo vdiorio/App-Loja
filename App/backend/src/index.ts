114ffac8f79a50a95d5f320d74c6fab910584fed
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-15 19:03:51 -0300

    FIX: Mudando origem para *

--- App/backend/src/index.ts
+++ App/backend/src/index.ts
@@ -9,7 +9,7 @@ const {PORT} = process.env;
 
 app.use(bodyParser.json());
 app.use(function(_req: Request, res: Response, next: NextFunction) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
