1024dedd7fba9c27642d328cd43acabda7bf4620
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-15 21:15:05 -0300

    FIX: Permissão de header authorization na politica CORS

--- App/backend/src/index.ts
+++ App/backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(function(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization');
   next();
 });
 app.use('/login', router.login);
